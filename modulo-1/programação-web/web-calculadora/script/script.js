b2d9ff7ecf7285830f968a63b11f31e26fdc5d39
Andre <petrellaandre@gmail.com>
2025-04-25 22:02:37 -0300

    Aletração

--- modulo-1/programação-web/web-calculadora/script/script.js
+++ modulo-1/programação-web/web-calculadora/script/script.js
@@ -1,17 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
     var num1 = document.getElementById("n1");
     var num2 = document.getElementById("n2");
+    const resultado = document.getElementById("resultado");
     const button = document.getElementById("somar");
 
     button.addEventListener("click", function() {
-    if (num1 === "" || num2 === "") {
-        document.getElementById("resultado").textContent = "ERROR!!!";
-    }
-    else {
-        var soma = somar(Number(num1.value), Number(num2.value));
-        document.getElementById("resultado").textContent = soma;
-    }
-});
+
+        const val1 = parseFloat(num1.value);
+        const val2 = parseFloat(num2.value);
+
+        if (!isNaN(val1)) {
+            num1.style.backgroundColor = "#5CE65C";
+        } else {
+            num1.style.backgroundColor = "#FF2C2C";
+        }
+
+        if (!isNaN(val2)) {
+            num2.style.backgroundColor = "#5CE65C";
+        } else {
+            num2.style.backgroundColor = "#FF2C2C";
+        }
+
+        if (!isNaN(val1) && !isNaN(val2)) {
+            const soma = val1 + val2;
+            resultado.style.fontFamily = "Old English Text MT"
+            resultado.textContent = soma;
+        } else {
+            
+            resultado.style.fontFamily = "Lucida Handwriting";
+            resultado.textContent = "Digite valores válidos.";
+        }
+
+    });
 });
 
 function somar(n1, n2) {
